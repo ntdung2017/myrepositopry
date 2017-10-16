2e7352bdd64693dee72cdd86911c44548e85e928
ntdung2017 <31035775+ntdung2017@users.noreply.github.com>
2017-10-16 09:02:24 +0700

    first

---
+++ myproject/myproject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Xin chao cac Ban.");
+            Console.ReadLine();
+        }
+    }
+}
